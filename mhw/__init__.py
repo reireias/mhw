dc897bea74f75efbc02f03a8e72da9f0238a0f05
reireias <reireias@gmail.com>
2018-03-26 22:57:58 +0900

    version 0.11: add monster data

--- mhw/__init__.py
+++ mhw/__init__.py
@@ -3,4 +3,5 @@ MHW utility tools package
 """
 from .damage import Condition, calculate
 from . import motionlist
+from . import monster
 from .util import generate_skill_patterns, generate_targets, skill_rank, to_label
